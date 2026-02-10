98d66a93738836c27304c86f52b05e4a1c0bff13
thesajidahmad <101032376+thesajidahmad@users.noreply.github.com>
2026-02-10 13:25:25 +0530

    reverse of array using while loop

--- 012_reverse_array_recursion.py
+++ 012_reverse_array_recursion.py
@@ -8,3 +8,17 @@ def reverseArr(num, left, right):
 
 reverseArr(num, 0, len(num)-1)
 print(num)
+
+
+#using while loop
+
+arr = [4, 3, 2, 1, 0, 9]
+
+l = 0
+r = len(arr)-1
+
+while(l <= r):
+    arr[l], arr[r] = arr[r], arr[l]
+    l +=1
+    r -=1
+print(arr)
